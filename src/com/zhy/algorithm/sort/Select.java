8b3a07c23dd8a254ee4144e970f52c960a0d9119
servanter <fengshang@126.com>
2014-09-05 15:24:01 +0800

    commit directly select sort.
    Signed-off-by: servanter <fengshang@126.com>

--- src/com/zhy/algorithm/sort/Select.java
+++ src/com/zhy/algorithm/sort/Select.java
@@ -1,12 +1,20 @@
 package com.zhy.algorithm.sort;
 
+import java.util.Arrays;
+
 public class Select {
 
     public static void main(String[] args) {
-        int[] a = new int[]{7,5,2,6,3,9,8,1};
-        select(a);
+        int[] src = new int[]{7,5,2,6,3,9,8,1};
+        directlySelect(src);
+        System.out.println(Arrays.toString(src));
     }
     
+    /**
+     * select sort.
+     * 
+     * @param src
+     */
     public static void select(int src[]) {
         for(int i = 0; i < src.length; i++) {
             for(int j = i + 1; j < src.length; j++) {
@@ -20,6 +28,26 @@ public class Select {
                 System.out.print(b + " ");
             }
             System.out.println();
+        }
+    }
+    
+    /**
+     * directly select sort.
+     * 
+     * @param src
+     */
+    public static void directlySelect(int src[]) {
+        for(int i = 0; i < src.length; i++) {
+            int temp = src[i];
+            int small = i;
+            for(int j = i + 1; j < src.length; j++) {
+                if(src[j] < temp) {
+                    small = j;
+                    temp = src[j];
+                }
+            }
+            src[small] = src[i];
+            src[i] = temp;
         }
     }
 }
