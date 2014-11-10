4b4815016cd2ae0d725733a884f8c511261a7cf4
hongyanzhang204660 <hongyanzhang204660@sohu-inc.com>
2014-11-10 09:31:44 +0800

    perfect code

--- src/com/zhy/algorithm/sort/Bubbling.java
+++ src/com/zhy/algorithm/sort/Bubbling.java
@@ -1,28 +1,24 @@
 package com.zhy.algorithm.sort;
 
+import java.util.Arrays;
+
 public class Bubbling {
 
     public static void main(String[] args) {
-        int[] a = new int[]{7,5,2,6,3,9,8,1};
+        int[] a = new int[] { 7, 5, 2, 6, 3, 9, 8, 1 };
         sort(a);
-//        for(int b : a) {
-//          System.out.print(b + " ");
-//      }
+        System.out.println(Arrays.toString(a));
     }
-    
+
     public static void sort(int src[]) {
-        for(int i = 0; i < src.length; i ++) {
-            for(int j = 0; j < src.length - 1; j++) {
-                if(src[j + 1] < src[j]) {
+        for (int i = 0; i < src.length; i++) {
+            for (int j = 0; j < src.length - 1; j++) {
+                if (src[j] > src[j + 1]) {
                     int temp = src[j];
                     src[j] = src[j + 1];
                     src[j + 1] = temp;
                 }
             }
-            for(int b : src) {
-                System.out.print(b + " ");
-            }
-            System.out.println();
         }
     }
 }
